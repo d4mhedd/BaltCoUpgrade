c435f1247c9b7397d2a43bbeaa5cb4470987bd38
Jeanne Chalk <jchalk@accela.com>
2017-10-16 13:43:49 -0400

    Animal Complaint Pageflow - Error item 2 from upgrade list

---
+++ SCRIPTS/PAGEFLOW/ACA_COMPLAINT_COMPINFO_BEFORE_V3.js
@@ -0,0 +1,137 @@
+/*------------------------------------------------------------------------------------------------------/
+| Program : ACA_COMPLAINT_COMPINFO_BEFORE_V3.js
+| Event   : ACA Page Flow Template
+|
+| Usage   : Designed for Pageflow: ENFORCE COMPLAINT, Complaint Information, BeforeClick Event
+|
+| Client  : Baltimore County
+| Action# : N/A
+|
+| Notes   : Rewritten to resolve architectural differences in Pageflow Table loading and written to 3.0
+|			EMSE Best Practices - 10.9.17 - jchalk
+|
+/------------------------------------------------------------------------------------------------------*/
+/*------------------------------------------------------------------------------------------------------/
+| START User Configurable Parameters
+|
+|     Only variables in the following section may be changed.  If any other section is modified, this
+|     will no longer be considered a "Master" script and will not be supported in future releases.  If
+|     changes are made, please add notes above.
+/------------------------------------------------------------------------------------------------------*/
+var showMessage = false; // Set to true to see results in popup window
+var showDebug = false; // Set to true to see debug messages in popup window
+var cancel = false;
+var message = ""; // Message String
+var debug = ""; // Debug String
+var br = "<BR>"; // Break Tag
+var cap = aa.env.getValue("CapModel");
+var capId = cap.getCapID();
+
+/*------------------------------------------------------------------------------------------------------/
+| END User Configurable Parameters
+/------------------------------------------------------------------------------------------------------*/
+var startDate = new Date();
+var startTime = startDate.getTime();
+var message = ""; // Message String
+var debug = ""; // Debug String
+var br = "<BR>"; // Break Tag
+
+loadASITables4ACA(cap);
+if (typeof(TYPEOFCOMPLAINT) != "object") {
+	showMessage = true;
+	comment("An entry is required in the Type of Complaint table before submitting the new Record");
+	cancel = true;
+}
+
+// page flow custom code end
+
+
+if (debug.indexOf("**ERROR") > 0) {
+	aa.env.setValue("ErrorCode", "1");
+	aa.env.setValue("ErrorMessage", debug);
+} else {
+	if (cancel) {
+		aa.env.setValue("ErrorCode", "-2");
+		if (showMessage)
+			aa.env.setValue("ErrorMessage", message);
+		if (showDebug)
+			aa.env.setValue("ErrorMessage", debug);
+	} else {
+		aa.env.setValue("ErrorCode", "0");
+		if (showMessage)
+			aa.env.setValue("ErrorMessage", message);
+		if (showDebug)
+			aa.env.setValue("ErrorMessage", debug);
+	}
+}
+
+/*------------------------------------------------------------------------------------------------------/
+| <===========External Functions (used by Action entries)
+/------------------------------------------------------------------------------------------------------*/
+
+function loadASITables4ACA(iCap) {
+
+	var gm = iCap.getAppSpecificTableGroupModel();
+
+	for (var ta = gm.getTablesMap(), tai = ta.values().iterator(); tai.hasNext(); ) {
+
+		var tsm = tai.next();
+
+		if (!tsm.rowIndex.isEmpty()) {
+
+			var tempObject = new Array,
+			tempArray = new Array,
+			tn = tsm.getTableName();
+			tn = String(tn).replace(/[^a-zA-Z0-9]+/g, ""),
+			isNaN(tn.substring(0, 1)) || (tn = "TBL" + tn);
+			for (var tsmfldi = tsm.getTableField().iterator(), tsmcoli = tsm.getColumns().iterator(), numrows = 1; tsmfldi.hasNext(); ) {
+
+				if (!tsmcoli.hasNext()) {
+					var tsmcoli = tsm.getColumns().iterator();
+					tempArray.push(tempObject);
+					var tempObject = new Array;
+					numrows++
+				}
+				var tcol = tsmcoli.next();
+				var tobj = tsmfldi.next();
+
+				var tval = "";
+				try {
+					tval = tobj.getInputValue();
+				} catch (ex) {
+					tval = tobj;
+				}
+
+				tempObject[tcol.getColumnName()] = tval;
+			}
+			tempArray.push(tempObject);
+
+			var copyStr = "" + tn + " = tempArray";
+			logDebug("ASI Table Array : " + tn + " (" + numrows + " Rows)"),
+
+			eval(copyStr)
+		}
+	}
+
+}
+
+function comment(cstr) {
+	if (showDebug)
+		logDebug(cstr);
+	if (showMessage)
+		logMessage(cstr);
+}
+
+function logDebug(dstr) {
+	vLevel = 1
+		if (arguments.length > 1)
+			vLevel = arguments[1];
+		if ((showDebug & vLevel) == vLevel || vLevel == 1)
+			debug += dstr + br;
+		if ((showDebug & vLevel) == vLevel)
+			aa.debug(aa.getServiceProviderCode() + " : " + aa.env.getValue("CurrentUserID"), dstr);
+}
+
+function logMessage(dstr) {
+	message += dstr + br;
+}
