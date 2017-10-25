32f7a02d20f27a84daac5b8894f40a7ef4e1ae49
Jeanne Chalk <jchalk@accela.com>
2017-10-25 17:16:57 -0400

    updating INCLUDES_CUSTOM_GLOBALS for Baltimore

--- SCRIPTS/MASTERSCRIPTS/INCLUDES_CUSTOM_GLOBALS.js
+++ SCRIPTS/MASTERSCRIPTS/INCLUDES_CUSTOM_GLOBALS.js
@@ -1,12 +1,7 @@
-//formerly ES_CW_DEBUGGROUPS
-//split into separate statements to support specific bizserver log debug (showDebug = 3) by individual as needed
+//formerly ES_CW_DEBUGGROUPS//split into separate statements to support specific bizserver log debug (showDebug = 3) by individual as needed
 
-if (currentUserID == "AWILHELM") showDebug = true;
-if (currentUserID == "LMOREDA") showDebug = true;
-if (currentUserID == "JWARNER") showDebug = true;
-if (currentUserID == "MLOUIS") 	showDebug = true;
+
 if (currentUserID == "JCHALK")  showDebug = true;
 if (currentUserID == "DHEDD")   showDebug = true;
 
-//testMasterAddress = "Lydia.Moreda@myClearwater.com";
-testMasterAddress = "jchalk@accela.com; Lydia.Moreda@myClearwater.com";
+
